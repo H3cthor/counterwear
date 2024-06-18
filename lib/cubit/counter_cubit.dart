e4aa0aad6f5deb4933530a579abb844a03d26750
Hector Saldaña <hectorsalbe@hotmail.com>
2024-06-18 16:32:25 -0600

    first commit

---
+++ lib/cubit/counter_cubit.dart
@@ -0,0 +1,37 @@
+import 'package:bloc/bloc.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class CounterCubit extends Cubit<int> {
+  CounterCubit() : super(0);
+
+  // void increment() => emit(state + 1);
+  // void decrement() => emit(state - 1);
+
+  void increment() {
+    if (state < 10) {
+      emit(state + 1);
+    } else {
+      Fluttertoast.showToast(
+        msg: 'Límite máximo alcanzado',
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+      );
+    }
+  }
+
+  void decrement() {
+    if (state > -10) {
+      emit(state - 1);
+    } else {
+      Fluttertoast.showToast(
+        msg: 'Límite mínimo alcanzado',
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+      );
+    }
+  }
+
+  void reset() {
+    emit(0);
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:wear/wear.dart';
+
+import 'counter/counter_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return AmbientMode(
+      child: const CounterPage(), //const MyHomePage(title: 'Flutter Demo'),
+      builder: (context, mode, child){
+        return MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            visualDensity: VisualDensity.compact,
+            colorScheme: mode == WearMode.active
+              ? const ColorScheme.dark(
+                primary: Color(0xFF00B5FF),
+              )
+              : const ColorScheme.dark(
+                primary: Colors.white24,
+                onBackground: Colors.white10,
+                onSurface: Colors.white10,
+              )
+          ),
+          home: child,
+        );
+      },
+    );
+    
+  }
+}
+
